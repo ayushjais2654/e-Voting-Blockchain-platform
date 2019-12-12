c3ddaa8cb4b085d49de1d7fcd11dce43a321dbca
ayush <ayushjais2654@gmail.com>
2019-12-12 10:16:07 +0530

    docker-compose-base

--- fabcar/startFabric.sh
+++ fabcar/startFabric.sh
@@ -48,8 +48,8 @@ echo y | ./byfn.sh up -a -n -s couchdb
 CONFIG_ROOT=/opt/gopath/src/github.com/hyperledger/fabric/peer
 ORG1_MSPCONFIGPATH=${CONFIG_ROOT}/crypto/peerOrganizations/org1.example.com/users/Admin@org1.example.com/msp
 ORG1_TLS_ROOTCERT_FILE=${CONFIG_ROOT}/crypto/peerOrganizations/org1.example.com/peers/peer0.org1.example.com/tls/ca.crt
-ORG2_MSPCONFIGPATH=${CONFIG_ROOT}/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
-ORG2_TLS_ROOTCERT_FILE=${CONFIG_ROOT}/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
+#ORG2_MSPCONFIGPATH=${CONFIG_ROOT}/crypto/peerOrganizations/org2.example.com/users/Admin@org2.example.com/msp
+#ORG2_TLS_ROOTCERT_FILE=${CONFIG_ROOT}/crypto/peerOrganizations/org2.example.com/peers/peer0.org2.example.com/tls/ca.crt
 ORDERER_TLS_ROOTCERT_FILE=${CONFIG_ROOT}/crypto/ordererOrganizations/example.com/orderers/orderer.example.com/msp/tlscacerts/tlsca.example.com-cert.pem
 set -x
 
@@ -66,18 +66,18 @@ docker exec \
     -p "$CC_SRC_PATH" \
     -l "$CC_RUNTIME_LANGUAGE"
 
-echo "Installing smart contract on peer0.org2.example.com"
-docker exec \
-  -e CORE_PEER_LOCALMSPID=Org2MSP \
-  -e CORE_PEER_ADDRESS=peer0.org2.example.com:9051 \
-  -e CORE_PEER_MSPCONFIGPATH=${ORG2_MSPCONFIGPATH} \
-  -e CORE_PEER_TLS_ROOTCERT_FILE=${ORG2_TLS_ROOTCERT_FILE} \
-  cli \
-  peer chaincode install \
-    -n fabcar \
-    -v 1.0 \
-    -p "$CC_SRC_PATH" \
-    -l "$CC_RUNTIME_LANGUAGE"
+#echo "Installing smart contract on peer0.org2.example.com"
+#docker exec \
+#  -e CORE_PEER_LOCALMSPID=Org2MSP \
+#  -e CORE_PEER_ADDRESS=peer0.org2.example.com:9051 \
+#  -e CORE_PEER_MSPCONFIGPATH=${ORG2_MSPCONFIGPATH} \
+#  -e CORE_PEER_TLS_ROOTCERT_FILE=${ORG2_TLS_ROOTCERT_FILE} \
+#  cli \
+#  peer chaincode install \
+#    -n fabcar \
+#    -v 1.0 \
+#    -p "$CC_SRC_PATH" \
+#    -l "$CC_RUNTIME_LANGUAGE"
 
 echo "Instantiating smart contract on mychannel"
 docker exec \
@@ -115,7 +115,7 @@ docker exec \
     --tls \
     --cafile ${ORDERER_TLS_ROOTCERT_FILE} \
     --peerAddresses peer0.org1.example.com:7051 \
-    --peerAddresses peer0.org2.example.com:9051 \
+    --peerAddresses peer1.org1.example.com:9051 \
     --tlsRootCertFiles ${ORG1_TLS_ROOTCERT_FILE} \
     --tlsRootCertFiles ${ORG2_TLS_ROOTCERT_FILE}
 set +x
