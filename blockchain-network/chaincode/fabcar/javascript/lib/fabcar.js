058373759103d772510238c6f1514bb0f62e1c74
ayush <ayushjais2654@gmail.com>
2019-12-30 16:32:39 +0530

    evoting smart contract not working

--- blockchain-network/chaincode/fabcar/javascript/lib/fabcar.js
+++ blockchain-network/chaincode/fabcar/javascript/lib/fabcar.js
@@ -1,15 +1,14 @@
-/*
- * SPDX-License-Identifier: Apache-2.0
- */
 
 'use strict';
 
 const { Contract } = require('fabric-contract-api');
+// const mongo = require('mongodb').MongoClient;
+
 
 class FabCar extends Contract {
 
     async initLedger(ctx) {
-        console.info('============= START : Initialize Ledger ===========');
+        console.log('============= START : Initialize Ledger ===========');
         const cars = [
             {
                 color: 'blue',
@@ -78,78 +77,114 @@ class FabCar extends Contract {
             await ctx.stub.putState('CAR' + i, Buffer.from(JSON.stringify(cars[i])));
             console.info('Added <--> ', cars[i]);
         }
-        console.info('============= END : Initialize Ledger ===========');
+        console.log('============= END : Initialize Ledger ===========');
     }
-
-    async queryCar(ctx, carNumber) {
-        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
-        if (!carAsBytes || carAsBytes.length === 0) {
-            throw new Error(`${carNumber} does not exist`);
-        }
-        console.log(carAsBytes.toString());
-        return carAsBytes.toString();
-    }
-
-    async createCar(ctx, carNumber, make, model, color, owner) {
-        console.info('============= START : Create Car ===========');
-
-        const car = {
-            color,
-            docType: 'car',
-            make,
-            model,
-            owner,
-        };
-
-        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
-        console.info('============= END : Create Car ===========');
-    }
-
-    async queryAllCars(ctx) {
-        const startKey = 'CAR0';
-        const endKey = 'CAR999';
-
-        const iterator = await ctx.stub.getStateByRange(startKey, endKey);
-
-        const allResults = [];
-        while (true) {
-            const res = await iterator.next();
-
-            if (res.value && res.value.value.toString()) {
-                console.log(res.value.value.toString('utf8'));
-
-                const Key = res.value.key;
-                let Record;
-                try {
-                    Record = JSON.parse(res.value.value.toString('utf8'));
-                } catch (err) {
-                    console.log(err);
-                    Record = res.value.value.toString('utf8');
-                }
-                allResults.push({ Key, Record });
-            }
-            if (res.done) {
-                console.log('end of data');
-                await iterator.close();
-                console.info(allResults);
-                return JSON.stringify(allResults);
-            }
-        }
-    }
-
-    async changeCarOwner(ctx, carNumber, newOwner) {
-        console.info('============= START : changeCarOwner ===========');
-
-        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
-        if (!carAsBytes || carAsBytes.length === 0) {
-            throw new Error(`${carNumber} does not exist`);
-        }
-        const car = JSON.parse(carAsBytes.toString());
-        car.owner = newOwner;
-
-        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
-        console.info('============= END : changeCarOwner ===========');
-    }
+    //
+    // async queryCar(ctx, carNumber) {
+    //     const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+    //     if (!carAsBytes || carAsBytes.length === 0) {
+    //         throw new Error(`${carNumber} does not exist`);
+    //     }
+    //     console.log(carAsBytes.toString());
+    //     return carAsBytes.toString();
+    // }
+    //
+    // async createCar(ctx, carNumber, make, model, color, owner) {
+    //     console.info('============= START : Create Car ===========');
+    //
+    //     const car = {
+    //         color,
+    //         docType: 'car',
+    //         make,
+    //         model,
+    //         owner,
+    //     };
+    //
+    //     await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+    //     console.info('============= END : Create Car ===========');
+    // }
+    //
+    // async queryAllCars(ctx) {
+    //     const startKey = 'CAR0';
+    //     const endKey = 'CAR999';
+    //
+    //     const iterator = await ctx.stub.getStateByRange(startKey, endKey);
+    //
+    //     const allResults = [];
+    //     while (true) {
+    //         const res = await iterator.next();
+    //
+    //         if (res.value && res.value.value.toString()) {
+    //             console.log(res.value.value.toString('utf8'));
+    //
+    //             const Key = res.value.key;
+    //             let Record;
+    //             try {
+    //                 Record = JSON.parse(res.value.value.toString('utf8'));
+    //             } catch (err) {
+    //                 console.log(err);
+    //                 Record = res.value.value.toString('utf8');
+    //             }
+    //             allResults.push({ Key, Record });
+    //         }
+    //         if (res.done) {
+    //             console.log('end of data');
+    //             await iterator.close();
+    //             console.info(allResults);
+    //             return JSON.stringify(allResults);
+    //         }
+    //     }
+    // }
+    //
+    // async changeCarOwner(ctx, carNumber, newOwner) {
+    //     console.info('============= START : changeCarOwner ===========');
+    //
+    //     const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+    //     if (!carAsBytes || carAsBytes.length === 0) {
+    //         throw new Error(`${carNumber} does not exist`);
+    //     }
+    //     const car = JSON.parse(carAsBytes.toString());
+    //     car.owner = newOwner;
+    //
+    //     await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+    //     console.info('============= END : changeCarOwner ===========');
+    // // }
+    // async initLedger(ctx) {
+    //
+    //         console.info('============= START : Initialize Ledger ===========');
+    //
+    //         let voter = {
+    //             firstName: 'Ayush',
+    //             lastName: 'Jaiswal',
+    //             username: 'ayush123',
+    //             password: 'ayush123',
+    //             mobileNo: 9515365125,
+    //             aadharCard: 123456789123,
+    //             votedTo: null,
+    //             transId: null,
+    //             docType: 'voter'
+    //         };
+    //
+    //
+    //         console.info('============= Voter Added to the WorldState ===========');
+    //         await ctx.stub.putState(voter.username, Buffer.from(JSON.stringify(voter)));
+    //
+    //         const url = 'mongodb://127.0.0.1:27017';
+    //         let client = await mongo.connect(url);
+    //         let results = await client.db('Candidate').collection('partyName').find().toArray();
+    //
+    //         for (let i = 0; i < results.length; i++) {
+    //             let ballot = {
+    //                 partyName: results[i].name,
+    //                 voteCount: 0,
+    //                 transIds: []
+    //             };
+    //             await ctx.stub.putState(ballot.partyName, Buffer.from(JSON.stringify(ballot)));
+    //             console.info('============= Party Added ===========');
+    //         }
+    //         await client.close();
+    //         console.info('============= END : Initialize Ledger ===========');
+    //     }
 
 }
 
---
+++ blockchain-network/chaincode/voting/javascript/lib/voting.js
@@ -0,0 +1,192 @@
+'use strict';
+
+const {Contract} = require('fabric-contract-api');
+const mongo = require('mongodb').MongoClient;
+
+
+class VotingContract extends Contract {
+
+    // async initLedger(ctx) {
+    //
+    //     console.info('============= START : Initialize Ledger ===========');
+    //
+    //     let voter = {
+    //         firstName: 'Ayush',
+    //         lastName: 'Jaiswal',
+    //         username: 'ayush123',
+    //         password: 'ayush123',
+    //         mobileNo: 9515365125,
+    //         aadharCard: 123456789123,
+    //         votedTo: null,
+    //         transId: null,
+    //         docType: 'voter'
+    //     };
+    //
+    //
+    //     console.info('============= Voter Added to the WorldState ===========');
+    //     await ctx.stub.putState(voter.username, Buffer.from(JSON.stringify(voter)));
+    //
+    //     const url = 'mongodb://127.0.0.1:27017';
+    //     let client = await mongo.connect(url);
+    //     let results = await client.db('Candidate').collection('partyName').find().toArray();
+    //
+    //     for (let i = 0; i < results.length; i++) {
+    //         let ballot = {
+    //             partyName: results[i].name,
+    //             voteCount: 0,
+    //             transIds: []
+    //         };
+    //         await ctx.stub.putState(ballot.partyName, Buffer.from(JSON.stringify(ballot)));
+    //         console.info('============= Party Added ===========');
+    //     }
+    //     await client.close();
+    //     console.info('============= END : Initialize Ledger ===========');
+    //
+    //     return voter;
+    // }
+    async initLedger(ctx) {
+        console.info('============= START : Initialize Ledger ===========');
+        const cars = [
+            {
+                color: 'blue',
+                make: 'Toyota',
+                model: 'Prius',
+                owner: 'Tomoko',
+            },
+            {
+                color: 'red',
+                make: 'Ford',
+                model: 'Mustang',
+                owner: 'Brad',
+            },
+            {
+                color: 'green',
+                make: 'Hyundai',
+                model: 'Tucson',
+                owner: 'Jin Soo',
+            },
+            {
+                color: 'yellow',
+                make: 'Volkswagen',
+                model: 'Passat',
+                owner: 'Max',
+            },
+            {
+                color: 'black',
+                make: 'Tesla',
+                model: 'S',
+                owner: 'Adriana',
+            },
+            {
+                color: 'purple',
+                make: 'Peugeot',
+                model: '205',
+                owner: 'Michel',
+            },
+            {
+                color: 'white',
+                make: 'Chery',
+                model: 'S22L',
+                owner: 'Aarav',
+            },
+            {
+                color: 'violet',
+                make: 'Fiat',
+                model: 'Punto',
+                owner: 'Pari',
+            },
+            {
+                color: 'indigo',
+                make: 'Tata',
+                model: 'Nano',
+                owner: 'Valeria',
+            },
+            {
+                color: 'brown',
+                make: 'Holden',
+                model: 'Barina',
+                owner: 'Shotaro',
+            },
+        ];
+
+        for (let i = 0; i < cars.length; i++) {
+            cars[i].docType = 'car';
+            await ctx.stub.putState('CAR' + i, Buffer.from(JSON.stringify(cars[i])));
+            console.info('Added <--> ', cars[i]);
+        }
+        console.info('============= END : Initialize Ledger ===========');
+    }
+
+    async queryCar(ctx, carNumber) {
+        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+        if (!carAsBytes || carAsBytes.length === 0) {
+            throw new Error(`${carNumber} does not exist`);
+        }
+        console.log(carAsBytes.toString());
+        return carAsBytes.toString();
+    }
+
+    async createCar(ctx, carNumber, make, model, color, owner) {
+        console.info('============= START : Create Car ===========');
+
+        const car = {
+            color,
+            docType: 'car',
+            make,
+            model,
+            owner,
+        };
+
+        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+        console.info('============= END : Create Car ===========');
+    }
+
+    async queryAllCars(ctx) {
+        const startKey = 'CAR0';
+        const endKey = 'CAR999';
+
+        const iterator = await ctx.stub.getStateByRange(startKey, endKey);
+
+        const allResults = [];
+        while (true) {
+            const res = await iterator.next();
+
+            if (res.value && res.value.value.toString()) {
+                console.log(res.value.value.toString('utf8'));
+
+                const Key = res.value.key;
+                let Record;
+                try {
+                    Record = JSON.parse(res.value.value.toString('utf8'));
+                } catch (err) {
+                    console.log(err);
+                    Record = res.value.value.toString('utf8');
+                }
+                allResults.push({ Key, Record });
+            }
+            if (res.done) {
+                console.log('end of data');
+                await iterator.close();
+                console.info(allResults);
+                return JSON.stringify(allResults);
+            }
+        }
+    }
+
+    async changeCarOwner(ctx, carNumber, newOwner) {
+        console.info('============= START : changeCarOwner ===========');
+
+        const carAsBytes = await ctx.stub.getState(carNumber); // get the car from chaincode state
+        if (!carAsBytes || carAsBytes.length === 0) {
+            throw new Error(`${carNumber} does not exist`);
+        }
+        const car = JSON.parse(carAsBytes.toString());
+        car.owner = newOwner;
+
+        await ctx.stub.putState(carNumber, Buffer.from(JSON.stringify(car)));
+        console.info('============= END : changeCarOwner ===========');
+    }
+
+}
+
+module.exports = VotingContract;
